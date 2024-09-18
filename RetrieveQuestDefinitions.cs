e0f6a71050451ee7a67cb04645520c65733a74b1
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-18 08:17:05 -0400

    Update RetrieveQuestDefinitions.cs

--- RetrieveQuestDefinitions.cs
+++ RetrieveQuestDefinitions.cs
@@ -1,7 +1,7 @@
-public static class QuestLoader
+public static class QuestLoader //This is a static class, meaning it cannot be instantiated and all its members must also be static.
 {
-    public static List<QuestScriptDef> GetAllQuestDefs()
+    public static List<QuestScriptDef> GetAllQuestDefs() //This static method returns a list of QuestScriptDef objects.
     {
-        return DefDatabase<QuestScriptDef>.AllDefsListForReading;
+        return DefDatabase<QuestScriptDef>.AllDefsListForReading; //This line accesses the DefDatabase class, which is a generic class that manages definitions (Defs) in RimWorld. DefDatabase<QuestScriptDef> specifically refers to the database of QuestScriptDef objects. AllDefsListForReading is a property that returns a list of all QuestScriptDef objects available in the game.
     }
 }
