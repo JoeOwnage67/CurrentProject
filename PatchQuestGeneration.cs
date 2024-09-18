44a07cd5e87d9546575ab78b8079d4820f79c002
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-18 08:04:13 -0400

    Create PatchQuestGeneration.cs

---
+++ PatchQuestGeneration.cs
@@ -0,0 +1,13 @@
+[HarmonyPatch(typeof(QuestManager), "GenerateQuest")]
+public static class QuestManager_GenerateQuest_Patch
+{
+    static bool Prefix(QuestScriptDef questDef, ref bool __result)
+    {
+        if (QuestFrequencyMod.settings.questEnabled.TryGetValue(questDef.defName, out bool enabled) && !enabled)
+        {
+            __result = false;
+            return false;
+        }
+        return true;
+    }
+}
