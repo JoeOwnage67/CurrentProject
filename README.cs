fd7fe9dca789e35f458e523b04729e17e135d907
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-19 18:19:57 -0400

    Update README.cs

--- README.cs
+++ README.cs
@@ -71,7 +71,6 @@ public static class QuestLoader
 }
 
 using HarmonyLib;
-using HarmonyLib;
 using Verse;
 using RimWorld;
 
@@ -81,12 +80,11 @@ public static class QuestManager_Add_Patch
 {
     static bool Prefix(Quest quest)
     {
-        Log.Message($"Patching Add for {quest.ToStringSafe()}");
         if (QuestFrequencyMod.settings.questEnabled.TryGetValue(quest.def.defName, out bool enabled) && !enabled)
         {
-            Log.Message($"Quest {quest.def.defName} is disabled by settings.");
             return false;
         }
         return true;
     }
 }
+
