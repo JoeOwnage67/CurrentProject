0556172461a043e3cd9ef7dcf346b74063566662
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-19 08:15:06 -0400

    Update README.cs

--- README.cs
+++ README.cs
@@ -74,11 +74,13 @@ using HarmonyLib;
 using Verse;
 using RimWorld;
 
-[HarmonyPatch(typeof(QuestManager), "GenerateQuest")]
+[HarmonyPatch(typeof(QuestManager))]
+[HarmonyPatch("GenerateQuest")]
 public static class QuestManager_GenerateQuest_Patch
 {
     static bool Prefix(QuestScriptDef questDef, ref bool __result)
     {
+        Log.Message($"Patching GenerateQuest for {questDef.defName}");
         if (QuestFrequencyMod.settings.questEnabled.TryGetValue(questDef.defName, out bool enabled) && !enabled)
         {
             __result = false;
