b37197a6acee77128bbfde5d766529367f0cc4f8
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-20 09:38:58 -0400

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,56 +1,74 @@
-//CurrentProject
-//the current thing I am developing
+//QuestControl
+//I want you to create a new mod called Quest Control for rimworld using C# and XML as necessary.
+//Here is what it should do:
+//1. I want the mod to collect all of the different QuestScriptDefs from rimworld's def dictionary and put them in a dictionary called QuestControlQuestList.
+//2. I want mod options labeled "Quest Control" that will appear in the Rimworld game menu, that will list all of the different QuestScriptDefs by their label, with a radio button next to each different one allowing the user to enable or disable that type of quest. 
+//3. I want the mod options written to the config file when the user clicks on a save button at the bottom of the options.
+//4. I want to make a harmony prefix patch that will disable or enable the QuestScriptDefs according to the config file using "com.joeownage.questcontrol".
 
-//QuestSettings.cs
+//CollectQuestScriptDefs.cs (checked against QuestScriptDef.cs and DefDatabase.cs and ModSettings.cs and Listing_Standard.cs and QuestUtility.cs and LoadedModManager.cs and Scribe_Collections.cs)
 using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
-public class QuestSettings : ModSettings
+public class QuestControl : Mod
 {
-    public Dictionary<string, bool> questEnabled = new Dictionary<string, bool>(); 
+    public static Dictionary<string, QuestScriptDef> QuestControlQuestList = new Dictionary<string, QuestScriptDef>();
 
-    public override void ExposeData() 
+    public QuestControl(ModContentPack content) : base(content)
     {
-        Scribe_Collections.Look(ref questEnabled, "questEnabled", LookMode.Value, LookMode.Value);
-        base.ExposeData();
+        foreach (var def in DefDatabase<QuestScriptDef>.AllDefs)
+        {
+            QuestControlQuestList[def.defName] = def;
+        }
     }
 }
 
-// /QuestFrequencyMod.cs
-using System.Collections.Generic;
+//CreateModOptions.cs (checked against QuestScriptDef.cs and DefDatabase.cs and ModSettings.cs and Listing_Standard.cs and QuestUtility.cs and LoadedModManager.cs and Scribe_Collections.cs)
 using UnityEngine;
 using Verse;
-using HarmonyLib;
 
-public class QuestFrequencyMod : Mod
+public class QuestControlSettings : ModSettings
 {
-    public static QuestSettings settings;
+    public Dictionary<string, bool> questEnabled = new Dictionary<string, bool>();
 
-    public QuestFrequencyMod(ModContentPack content) : base(content)
+    public override void ExposeData()
     {
-        settings = GetSettings<QuestSettings>();
-        
-        var harmony = new Harmony("com.joeownage.questcontrol");
-        harmony.PatchAll();
-		
-		// Log the methods of QuestManager
-		MethodLogger.LogMethods();
+        base.ExposeData();
+        Scribe_Collections.Look(ref questEnabled, "questEnabled", LookMode.Value, LookMode.Value);
+    }
+
+    public void Save()
+    {
+        Write();
+    }
+}
+
+public class QuestControlMod : Mod
+{
+    public static QuestControlSettings settings;
+
+    public QuestControlMod(ModContentPack content) : base(content)
+    {
+        settings = GetSettings<QuestControlSettings>();
     }
 
     public override void DoSettingsWindowContents(Rect inRect)
     {
         Listing_Standard listingStandard = new Listing_Standard();
         listingStandard.Begin(inRect);
+        listingStandard.Label("Quest Control");
 
-        var allQuestDefs = QuestLoader.GetAllQuestDefs();
-        foreach (var questDef in allQuestDefs) 
+        foreach (var quest in QuestControl.QuestControlQuestList)
         {
-            if (!settings.questEnabled.TryGetValue(questDef.defName, out bool enabled))
-            {
-                enabled = true;
-            }
-            listingStandard.CheckboxLabeled(questDef.label, ref enabled);
-            settings.questEnabled[questDef.defName] = enabled;
+            bool enabled = settings.questEnabled.ContainsKey(quest.Key) ? settings.questEnabled[quest.Key] : true;
+            listingStandard.CheckboxLabeled(quest.Value.label, ref enabled);
+            settings.questEnabled[quest.Key] = enabled;
+        }
+
+        if (listingStandard.ButtonText("Save"))
+        {
+            settings.Save();
         }
 
         listingStandard.End();
@@ -59,54 +77,29 @@ public class QuestFrequencyMod : Mod
 
     public override string SettingsCategory() => "Quest Control";
 }
-
-//QuestLoader.cs
-using System.Collections.Generic;
-using Verse;
-using RimWorld;
-
-public static class QuestLoader
-{
-    public static List<QuestScriptDef> GetAllQuestDefs()
-    {
-        return DefDatabase<QuestScriptDef>.AllDefsListForReading;
-    }
-}
-
-//CustomQuestGen.cs
-using RimWorld;
-using Verse;
-
-public static class CustomQuestGen
-{
-    public static void GenerateQuest(QuestScriptDef questScriptDef, Slate slate)
-    {
-        if (QuestFrequencyMod.settings.questEnabled.TryGetValue(questScriptDef.defName, out bool enabled) && !enabled)
-        {
-            Log.Message($"Quest {questScriptDef.defName} is disabled by settings.");
-            return;
-        }
-
-        Quest quest = QuestGen.Generate(questScriptDef, slate);
-        Find.QuestManager.Add(quest);
-    }
-}
-
-//QuestManager_Add_Patch.cs
+//HarmonyPrefixPatch.cs (checked against QuestScriptDef.cs and DefDatabase.cs and ModSettings.cs and Listing_Standard.cs and QuestUtility.cs and LoadedModManager.cs and Scribe_Collections.cs)
 using HarmonyLib;
 using Verse;
-using RimWorld;
 
-[HarmonyPatch(typeof(QuestManager))]
-[HarmonyPatch("Add")]
-public static class QuestManager_Add_Patch
+[StaticConstructorOnStartup]
+public static class QuestControlPatcher
 {
-    static bool Prefix(Quest quest)
+    static QuestControlPatcher()
     {
-        var slate = new Slate();
-        CustomQuestGen.GenerateQuest(quest.def, slate);
-        return false; // Prevent the original method from running
+        var harmony = new Harmony("com.joeownage.questcontrol");
+        harmony.PatchAll();
     }
 }
 
-
+[HarmonyPatch(typeof(QuestUtility), "GenerateQuestAndMakeAvailable")]
+public static class QuestUtility_GenerateQuestAndMakeAvailable_Patch
+{
+    public static bool Prefix(QuestScriptDef questDef)
+    {
+        if (QuestControlMod.settings.questEnabled.TryGetValue(questDef.defName, out bool enabled))
+        {
+            return enabled;
+        }
+        return true;
+    }
+}
