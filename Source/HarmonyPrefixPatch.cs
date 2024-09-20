30c9c479e857f9fca19c1e8e6c26c069e2ad6c2d
Joe Daly <110196881+JoeOwnage67@users.noreply.github.com>
2024-09-20 09:37:39 -0400

    Update and rename QuestLoaderClass.cs to HarmonyPrefixPatch.cs

---
+++ Source/HarmonyPrefixPatch.cs
@@ -0,0 +1,25 @@
+using HarmonyLib;
+using Verse;
+
+[StaticConstructorOnStartup]
+public static class QuestControlPatcher
+{
+    static QuestControlPatcher()
+    {
+        var harmony = new Harmony("com.joeownage.questcontrol");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(QuestUtility), "GenerateQuestAndMakeAvailable")]
+public static class QuestUtility_GenerateQuestAndMakeAvailable_Patch
+{
+    public static bool Prefix(QuestScriptDef questDef)
+    {
+        if (QuestControlMod.settings.questEnabled.TryGetValue(questDef.defName, out bool enabled))
+        {
+            return enabled;
+        }
+        return true;
+    }
+}
